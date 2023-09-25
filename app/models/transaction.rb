56f71131f52069864acc7ba0c9fd79a7f541997d
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-25 07:10:32 +0100

    Add attributes validations to Transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :user, foreign_key: 'author_id'
   has_and_belongs_to_many :categories
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :icon, presence: true
 end
