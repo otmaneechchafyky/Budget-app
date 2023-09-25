622068f0b127737b69e12afe95f200e804788012
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-25 07:11:10 +0100

    Add attributes validations to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_and_belongs_to_many :transactions
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :icon, presence: true
 end
