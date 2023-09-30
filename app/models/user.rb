32faeebeeae500169a83de528f2d346511793891
echchafykyotmane <otmaneechchafyky5@gmail.com>
2023-09-30 17:41:05 +0100

    Update user model to include the correct devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
+  has_many :groups, foreign_key: 'user_id'
+  has_many :records, foreign_key: 'user_id'
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories
-  has_many :transactions
-
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 end
